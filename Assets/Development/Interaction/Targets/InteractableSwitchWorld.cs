c796c33b7d61a3e18695abf7275aef2db76b7096
VITORNADO <vitoscutari1407@gmail.com>
2024-08-08 12:41:48 -0400

    Scene Switcher & Test Scene
    Added code to spawn to a specific location in another scene

--- Assets/Development/Interaction/Targets/InteractableSwitchWorld.cs
+++ Assets/Development/Interaction/Targets/InteractableSwitchWorld.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class InteractableSwitchWorld : MonoBehaviour
 {
+    public int doorNumber;
     public string Target;
     // Start is called before the first frame update
     public void SwitchScene()
     {
-        SceneManager.LoadScene(Target);
+
+        ScenesManager.Instance.LoadScene(doorNumber, Target);
     }
 }
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,19 +6,19 @@ using Yarn.Unity;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 playeroffset;
+    private Vector3 playeroffset = new Vector3(0, 7, 3);
     private Vector3 NPCoffset = new Vector3(-2, 2, 2);
     private GameObject NPCHead;
     private DialogueRunner dialogueRunner;
     private bool active = false;
-    private Quaternion LookAtPlayer = new Quaternion();
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        playeroffset = transform.position - player.transform.position;
+        player =GameObject.FindGameObjectWithTag("Player");
+       // playeroffset = transform.position - player.transform.position;
         dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
-        LookAtPlayer = transform.rotation;
     }
 
     // Update is called once per frame
@@ -27,7 +27,7 @@ public class CameraController : MonoBehaviour
         if (active == false)
         {
             transform.position = player.transform.position + playeroffset;
-            transform.rotation = LookAtPlayer;
+            transform.LookAt(player.transform);
         }
 
         if (dialogueRunner.IsDialogueRunning == false)
@@ -36,6 +36,11 @@ public class CameraController : MonoBehaviour
         }
     }
 
+    public void SwitchedScene()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        transform.position = player.transform.position + playeroffset;
+    }
     public void InteractNPC()
     {
         if (dialogueRunner.IsDialogueRunning)

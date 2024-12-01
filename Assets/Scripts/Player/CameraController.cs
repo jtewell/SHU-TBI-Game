0908cd9143d117d0637e85c17d79ce054785859b
VITORNADO <vitoscutari1407@gmail.com>
2024-12-01 13:28:26 -0500

    Updated PreFabs AND Fixed Quest & SceneSwitching
    Updated CameraController
    
    CameraController Script is now attached to the player in order to allow it to interact with 2 different cameras (MainCamera & NPCCamera). I also implemented the switching between the 2 different cameras.
    
    Prefabs for Controllers & Player

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -7,52 +7,45 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
     public GameObject playerCameraRoot;
-    private Vector3 playeroffset;
+    private Vector3 playeroffset = new Vector3(0, 5, 6);
     private Vector3 NPCoffset = new Vector3(-2, 2, 2);
     private GameObject NPCHead;
-    public DialogueRunner dialogueRunner;
-    private bool dialogCamActive = false;
-    
+    public Camera MainCamera;
+    public Camera NPCCamera;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-       // playeroffset = transform.position - player.transform.position;
-        dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
-        playeroffset = transform.localPosition;
-        
+        NPCCamera.transform.position = transform.position + NPCoffset;
+        NPCCamera.enabled = false;
+        MainCamera.enabled = true;
+
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        if (dialogueRunner.IsDialogueRunning== false)
-        {
-            transform.position = player.transform.position + playeroffset;
-           // transform.LookAt(playerCameraRoot.transform);
-        }
+           MainCamera.transform.position = player.transform.position + playeroffset; 
+           MainCamera.transform.LookAt(playerCameraRoot.transform);
         
     }
-
-    /*public void OnDialogFinish()
-    {
-        dialogCamActive = false;
-    } */
-
     public void SwitchedScene()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        transform.position = player.transform.position + playeroffset;
+        MainCamera.transform.position = transform.position + playeroffset;
+        NPCCamera.transform.position = transform.position + NPCoffset;
     }
-    public void InteractNPC()
+    public void ActivateNPCCamera()
     {
-        if (dialogueRunner.IsDialogueRunning)
-        {
-            NPCHead = GameObject.FindGameObjectWithTag("NPCHead");
-            transform.position = player.transform.position + NPCoffset;
-            transform.LookAt(NPCHead.transform);
-        }
-       // dialogCamActive = true;
+        MainCamera.enabled = false;
+        NPCCamera.enabled = true;
+        NPCHead = GameObject.FindGameObjectWithTag("NPCHead");
+        NPCCamera.transform.LookAt(NPCHead.transform);
+    }
+    public void DeactivateNPCCamera()
+    {
+        NPCCamera.enabled = false;
+        MainCamera.enabled = true;
     }
 }

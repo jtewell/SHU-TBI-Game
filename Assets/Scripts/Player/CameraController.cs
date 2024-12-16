aae9f182efcacbc3f159e62c62ca24efcb7c7cec
jtewell <jtewell@users.noreply.github.com>
2024-12-16 14:12:17 -0500

    Scene Merge Fixes
    -Fixed NPC camera
    -Renamed PlayerArmature prefab to Player
    -Removed Overlay and Dialog System from Player Prefab
    -Reorganized TestScene hierarchy
    -Moved CameraController to Player prefab
    -Refactored CameraController script
    -Readded Overlay System object to Town Scene

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -5,44 +5,44 @@ using Yarn.Unity;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
-    public GameObject playerCameraRoot;
-    private Vector3 playeroffset = new Vector3(0, 5, 6);
-    private Vector3 NPCoffset = new Vector3(-2, 2, 2);
-    private GameObject NPCHead;
-    public Camera MainCamera;
-    public Camera NPCCamera;
+    //public GameObject player;
+    //public GameObject playerCameraRoot;
+    //private Vector3 playeroffset = new Vector3(0, 5, 6);
+    //private Vector3 NPCoffset = new Vector3(-2, 2, 2);
+    //private GameObject NPCHead;
+
+    private Camera MainCamera;
+    private Camera NPCCamera;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        NPCCamera.transform.position = transform.position + NPCoffset;
+        //Get the camera references
+        MainCamera = transform.Find("MainCamera").GetComponent<Camera>();
+        NPCCamera = transform.Find("DialogCamera").GetComponent<Camera>();
+
+        //NPCCamera.transform.position = transform.position + NPCoffset;
         NPCCamera.enabled = false;
         MainCamera.enabled = true;
 
     }
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-           MainCamera.transform.position = player.transform.position + playeroffset; 
-           MainCamera.transform.LookAt(playerCameraRoot.transform);
-        
-    }
     public void SwitchedScene()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        MainCamera.transform.position = transform.position + playeroffset;
-        NPCCamera.transform.position = transform.position + NPCoffset;
+        //player = GameObject.FindGameObjectWithTag("Player");
+        //MainCamera.transform.position = transform.position + playeroffset;
+        //NPCCamera.transform.position = transform.position + NPCoffset;
     }
+
     public void ActivateNPCCamera()
     {
         MainCamera.enabled = false;
         NPCCamera.enabled = true;
-        NPCHead = GameObject.FindGameObjectWithTag("NPCHead");
-        NPCCamera.transform.LookAt(NPCHead.transform);
+        //NPCHead = GameObject.FindGameObjectWithTag("NPCHead");
+        //NPCCamera.transform.LookAt(NPCHead.transform);
     }
+
     public void DeactivateNPCCamera()
     {
         NPCCamera.enabled = false;

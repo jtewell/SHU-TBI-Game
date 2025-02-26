d380dcd44a7d30b64c726560cb4bd472d8c685c1
XpSalamander <sal.scurti04@icloud.com>
2025-02-26 13:05:01 -0500

    Avatar selection done but needs some tweeks
    As stated avatar selection is done with simple implementation as well the problem is i don't have the correct textures for the avatars so i chose the textures we had as replacement until we get it fixed

--- Assets/Scripts/Avatartextures.cs
+++ Assets/Scripts/Avatartextures.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Avatartextures: MonoBehaviour
 {
-    public Texture[] texture;
+    public Material facematerial;
+    public Material bodymaterial;
+    public Texture[] textureface;
+    public Texture[] texturebody;
     public AvatarSciptableObject avi;
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,32 @@ public class Avatartextures: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (avi.Name == "avatar1Container")
+        {
+            facematerial.mainTexture = textureface[3];
+            bodymaterial.mainTexture = texturebody[3];
+        }
+        else if (avi.Name == "avatar2Container")
+        {
+            facematerial.mainTexture = textureface[3];
+            bodymaterial.mainTexture = texturebody[3];
+        }
+        else if (avi.Name == "avatar3Container")
+        {
+            facematerial.mainTexture = textureface[2];
+            bodymaterial.mainTexture = texturebody[2];
+        }
+        else if (avi.Name == "avatar4Container")
+        {
+            facematerial.mainTexture = textureface[1];
+            bodymaterial.mainTexture = texturebody[1];
+        }
+        else if (avi.Name == "avatar5Container")
+        {
+            facematerial.mainTexture = textureface[0];
+            bodymaterial.mainTexture = texturebody[0];
+        }
+
+
     }
 }

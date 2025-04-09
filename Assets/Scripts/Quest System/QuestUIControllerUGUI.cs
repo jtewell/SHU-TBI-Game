34248a2a037b4bc677f3ade4bd1fe5aa388e77cc
Kitsune <kitsune@akitsune.dev>
2025-04-09 07:43:15 -0400

    🐛 Fix Quest UI not updating.
    We really cannot be using a TMP DropDown for something that isn't a DropDown.

--- Assets/Scripts/Quest System/QuestUIControllerUGUI.cs
+++ Assets/Scripts/Quest System/QuestUIControllerUGUI.cs
@@ -5,49 +5,76 @@ using Development.QuestsSystem.UI;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.EventSystems;
+using Object = UnityEngine.Object;
 
-public class QuestUIControllerUGUI : MonoBehaviour
+public class QuestUIControllerUGUI : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
     public TMP_Text questTitle;
-    public TMP_Dropdown questStepDropdown;
+    public GameObject stepList;
+    public GameObject contentHolder;
+    public GameObject itemTemplate;
     public Sprite checkmarkOn;
     public Sprite checkmarkOff;
 
     private void Start()
     {
         RenderQuest();
+        if (QuestManager.Instance == null)
+        {
+            Debug.LogError("Quest Manager is null. Cannot subscribe for future rerenders");
+            return;
+        }
+        QuestManager.Instance.onFinishStep.AddListener(RenderQuest);
+        stepList.SetActive(false);
+        
     }
 
     public void RenderQuest()
     {
-        Debug.Log("Hi");
+        Debug.Log("Quest UI Controller at " + gameObject.name + " is rendering quest ");
         if (QuestManager.Instance == null) return;
-        questStepDropdown.ClearOptions();
-
-        List<TMP_Dropdown.OptionData> stepList = new List<TMP_Dropdown.OptionData>();
+        foreach (var child in contentHolder.transform)
+        {
+            if(child is RectTransform rectTransform) Destroy(rectTransform.gameObject);
+        }
+        
 
         foreach (var step in QuestManager.Instance.currentQuest.steps)
         {
-            Sprite checkmark;
-            if (step.IsCompleted == true)
-            {
-                checkmark = checkmarkOn;
-            }
-            else
-            {
-                checkmark = checkmarkOff;
-            }
-
-            TMP_Dropdown.OptionData option = new TMP_Dropdown.OptionData(step.InstructionText, checkmark);
-            stepList.Add(option);
+            GameObject stepItem = Instantiate(itemTemplate, contentHolder.transform);
+            Image checkmark = stepItem.GetComponentInChildren<Image>();
+            TMP_Text stepText = stepItem.GetComponentInChildren<TMP_Text>();
+            stepText.text = step.InstructionText;
+            Debug.Log("Adding step " + step.InstructionText + " (" + step.IsCompleted + ")");
+            checkmark.sprite = step.IsCompleted ? checkmarkOn : checkmarkOff;
+            stepItem.SetActive(true);
+            
 
         }
-
-        questStepDropdown.AddOptions(stepList);
+        
     }
+    
+    
 
     public void CheckTodoList ()
     {
         RenderQuest();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        
+    }
+
+    public void ButtonClicked()
+    {
+        stepList.SetActive(!stepList.activeInHierarchy);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        stepList.SetActive(false);
     }
 }
